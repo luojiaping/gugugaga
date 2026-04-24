3046b406e7675b2d5aceac7aa434bfa6e9f85f7d
luojiaping <75722052+luojiaping@users.noreply.github.com>
2026-04-25 05:57:33 +0800

    fix: add missing required parameters in widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,7 +4,11 @@ import 'package:gugugaga/main.dart';
 
 void main() {
   testWidgets('App renders correctly', (WidgetTester tester) async {
-    await tester.pumpWidget(const GuGuGaGaApp(serverUrl: ''));
+    await tester.pumpWidget(const GuGuGaGaApp(
+      serverUrl: '',
+      username: '',
+      password: '',
+    ));
     expect(find.byType(MaterialApp), findsOneWidget);
   });
 }
